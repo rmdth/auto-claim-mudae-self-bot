faa77e09708999c6c846341d5d59918488539f68
ram <166089650+rmdth@users.noreply.github.com>
2026-01-30 00:25:19 -0600

    refactor: move cooldown logic to logic.py

--- src/core/models.py
+++ src/core/models.py
@@ -15,33 +15,13 @@ class Cooldown:
     def set_cooldown(self, seconds: float, current_time: float) -> None:
         self.ready_at = current_time + seconds
 
-    @staticmethod
-    def next_claim(now: datetime, minute_reset: int = 0, shifthour: int = 0) -> float:
-        if not 0 <= minute_reset <= 59:
-            raise ValueError("minute_reset must be between 0 and 59 inclusive")
 
         shifted_now = now - timedelta(hours=shifthour)
 
-        if (
-            shifted_now.hour % 3 == 1
-            and shifted_now.minute == minute_reset
-            and shifted_now.second == 0
-            and shifted_now.microsecond == 0
-        ):
-            return 0.0
-
-        time = shifted_now.replace(minute=0, second=0, microsecond=0)
-        hours_to_add = (1 - time.hour) % 3
-        time += timedelta(hours=hours_to_add)
-        time = time.replace(minute=minute_reset)
-
-        if time <= shifted_now:
-            time += timedelta(hours=3)
-
-        actual_time = time + timedelta(hours=shifthour)
-        return (actual_time - now).total_seconds()
 
 
-@dataclass
-class Kakera:
-    pass
+
+
+    def regen(self) -> None:
+        self.curr_value = min(self.max_value, self.curr_value + 1)
+
