ec32798daa4d355a086baea7e9bfc03151a0b9d4
ram <166089650+rmdth@users.noreply.github.com>
2026-02-05 22:05:57 -0600

    refactor: rename of keys + use one level dict insted of two level for settings

--- main.py
+++ main.py
@@ -6,16 +6,9 @@ from src.mudae_bot import MudaeBot
 
 settings = load(open("./bot-settings.toml", "rb"))
 
-channels_information = {}
+channels_information = settings["channels_information"]
 utc_delta: float = settings["UTC_delta"]
 TIME_ZONE: timezone = timezone(timedelta(hours=utc_delta))
-
-
-for information in settings["channels_information"]:
-    channels_information[information["id"]] = {
-        "id": information["id"],
-        "settings": information["settings"],
-    }
 
 bot = MudaeBot(
     timezone=TIME_ZONE,
