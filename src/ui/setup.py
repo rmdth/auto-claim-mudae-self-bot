8fbb5ae54ca6e0d01a7943448635a71ea94d5919
ram <166089650+rmdth@users.noreply.github.com>
2026-07-20 15:00:50 -0600

    add: configurable channel logs length

--- src/ui/setup.py
+++ src/ui/setup.py
@@ -1,4 +1,5 @@
 from collections import deque
+from tomllib import load
 from typing import MutableSequence
 
 from attr import dataclass
@@ -18,11 +19,15 @@ page_id_map: dict[int, int] = {}
 bot_status: dict[str, str] = {}
 channel_page_uis: dict[int, ChannelUIInfo] = {}
 
+with open("./bot-settings.toml", "rb") as f:
+    settings = load(f)
+    MUDAE_LOG_LENGTH = settings.get("mudae_log_length", 15)
+
 
 def setup_ui(channel_ids: tuple[int, ...]) -> None:
     global channel_page_uis, max_pages, page_id_map
     for i, channel_id in enumerate(channel_ids):
         page_id_map[i] = channel_id
-        channel_page_uis[channel_id] = ChannelUIInfo({}, deque(maxlen=15))
+        channel_page_uis[channel_id] = ChannelUIInfo({}, deque(maxlen=MUDAE_LOG_LENGTH))
 
     max_pages = len(page_id_map)
